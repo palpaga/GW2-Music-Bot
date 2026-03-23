9d947873b222a4874b85ad7965217b9686c4e636
Lama <9804647+palpaga@users.noreply.github.com>
2026-03-23 12:19:54 +0100

    Clean up repository (remove test scripts, ignore user data)

--- test.ps1
+++
@@ -1,4 +0,0 @@
-$client = New-Object System.Net.WebClient
-$client.Headers.Add('User-Agent', 'Mozilla/5.0')
-$html = $client.DownloadString('https://bitmidi.com/search?q=zelda')
-$html -split '\n' | Where-Object { $_ -match 'href=.*-mid' } | Select-Object -First 5
